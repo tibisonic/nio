61f9475fb65f396ddcb2c61d2063c8c373103559
t.iurco <t.iurco@sonicmoov.com>
2026-04-21 15:17:34 +0900

    feat: added leetcode

---
+++ after/plugin/leet.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<leader>ll", ":Leet<cr>")
+vim.keymap.set("n", "<leader>lr", ":Leet run<cr>")
+vim.keymap.set("n", "<leader>ls", ":Leet submit<cr>")
+vim.keymap.set("n", "<leader>lo", ":Leet list<cr>")
