2c1e97d0d6ae1055e0a12daadbd510891f8fc2b9
t.iurco <t.iurco@sonicmoov.com>
2026-04-21 15:21:45 +0900

    fix: improved keymaps for leetcode

--- after/plugin/leet.lua
+++ after/plugin/leet.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>ll", ":Leet<cr>")
-vim.keymap.set("n", "<leader>lr", ":Leet run<cr>")
-vim.keymap.set("n", "<leader>ls", ":Leet submit<cr>")
-vim.keymap.set("n", "<leader>lo", ":Leet list<cr>")
+vim.keymap.set("n", "<leader>ll", "<cmd>Leet<cr>")
+vim.keymap.set("n", "<leader>lr", "<cmd>Leet run<cr>")
+vim.keymap.set("n", "<leader>ls", "<cmd>Leet submit<cr>")
+vim.keymap.set("n", "<leader>lo", "<cmd>Leet list<cr>")
